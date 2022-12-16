7d848d3d476641f5d039295448d4b3963d7dc5fd
ongraphpythondev <pythondev@ongraph.com>
2022-12-16 15:02:40 +0530

    updated readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# Google Similar Image
+## In this project we can get the similar image from the google using google lens 
+## In this project we are scrapping the similar image from google using selenium
+
+## Clone project use follwing command
+```
+git clone https://github.com/ongraphpythondev/google-similar-image.git
+```
+
+## Create a virtual enviorment and activate it
+```
+python -m venv venv
+source venv/bin/activate
+```
+
+## Install dependencies to run the project
+```
+pip install -r requirments.txt
+```
+
+## Run the project
+```
+python main.py
+```
+
+## visit the page 
+```
+127.0.0.1:5000/image
+```
+
+## choose the image 
--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-#/home/ongraph/Desktop/googl/download_image/media/uploads/Yael-Shelbia-846x1024_1.webp
 app = Flask(__name__, static_url_path="/static", static_folder="static")
 app.config['UPLOAD_FOLDER'] = os.path.abspath(os.getcwd())+"/static/media"
 updir = app.config["UPLOAD_FOLDER"]
@@ -25,9 +24,6 @@ def upload_file():
         img.save(path)
         print("*************************",path)
         print(path)
-        # if img:
-        #     Save_img.objects.create(upload=img)
-        # print(request.POST.FILES('myImage').__dict__)
         
         options = FirefoxOptions()
         options.add_argument("--headless")
@@ -36,15 +32,11 @@ def upload_file():
         url = "https://www.google.com/imghp?hl=en"
         driver.get(url)
         print(driver.title)
-        # time.sleep(15)
         driver.maximize_window()
-        # WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CLASS_NAME,'Gdd5U'))).click()
         driver.find_element(By.CLASS_NAME, 'Gdd5U').click()
-        # driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[3]/div[3]/img').click()
         time.sleep(2)
 
         container = driver.find_element(By.NAME, 'encoded_image')
-        # img = Save_img.objects.first()
         container.send_keys(path)
 
         time.sleep(5)
@@ -67,9 +59,6 @@ def upload_file():
         print("lent", len(new))
         for i in new:
             print(i)
-        
-        # img.delete()
-
         driver.close()
 
         return jsonify({"data":new})
